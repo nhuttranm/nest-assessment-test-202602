3936688d582961a907090247c7c0758810b6dcdc
nicktran <nick.tran@metacrew.vn>
2026-02-13 17:59:41 +0700

    chore: update migration for create table command

--- src/migrations/CreateUserPairsTable1707734400000.ts
+++ src/migrations/CreateUserPairsTable1707734400000.ts
@@ -5,7 +5,7 @@ export class CreateUserPairsTable1707734400000 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
-            CREATE TABLE \`user_pairs\` (
+            CREATE TABLE IF NOT EXISTS \`user_pairs\` (
                 \`id\` char(36) NOT NULL,
                 \`id1\` char(36) NOT NULL,
                 \`id2\` char(36) NOT NULL,
